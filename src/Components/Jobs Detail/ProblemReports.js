56cf2fb4414234287280dc441d64352a7ba29611
abdullahnaveed011 <141331194+abdullahnaveed011@users.noreply.github.com>
2023-11-28 19:55:09 +0500

    changes

--- src/Components/Jobs Detail/ProblemReports.js
+++ src/Components/Jobs Detail/ProblemReports.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View , ActivityIndicator} from 'react-native'
+import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity , ScrollView  } from 'react-native'
 import React, { useState, useEffect } from 'react'
+import { MaterialIcons } from '@expo/vector-icons';
 import Color from '../../Color'
 const ProblemReports = () => {
   const [loading, setLoading]=useState(true)
-    const data=[
-        {
-          heading:"Quote responsibility",
-          text:"Admin test"
-        }
-    ]
+   
+  const data=[
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent",
+        open:true,
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent",
+        open:false,
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Seven  Trent",
+        open: false
+    }, 
+
+]
 
      /* Remove this when fethc data */
     useEffect(()=>{
@@ -18,12 +32,36 @@ const ProblemReports = () => {
     },[])
     
   return (
-    <View>
-        
+    <View>    
      {
         !loading ? 
         <View style={styles.Container}>
-         <Text style={{color:"black"}}>ProblemReports</Text>   
+            <View style={styles.headerContainer}> 
+               <Text style={styles.text1}>Subject</Text>
+               <Text style={styles.text2}>Job</Text>
+            </View>   
+               {/* All Data */}
+            <View style={styles.allData} >
+              <ScrollView style={{flexGrow:1}}>
+                {
+                  data.map((item,index)=>
+                  {
+                      return(
+                          <TouchableOpacity activeOpacity={0.6} key={index}>
+                              <View style={[styles.mainIndividual,{backgroundColor:index%2==0 ? '#D2CBBC' : '#F2F1CF'}]}>
+                                <View style={styles.individual}>
+                                  <Text style={styles.dataText}>{item.jobName}</Text>
+                                  <Text style={styles.dataText2}>{item.company}</Text> 
+                                  <MaterialIcons name={'keyboard-arrow-right'} size={28} color="black" />
+                              </View>
+                                <Text style={styles.opentext}>{`${item.open ?  'Open' : ''}`}</Text>
+                              </View>
+                          </TouchableOpacity>
+                      )
+                  })
+                }
+              </ScrollView>
+            </View>
          </View>
       :
       <View style={styles.Indicator}>
@@ -42,7 +80,7 @@ const styles = StyleSheet.create({
 
     Container:{
         height: '100%',
-        backgroundColor:Color.brightOrange
+        backgroundColor:"white"
     },
     Indicator:
     {
@@ -55,5 +93,55 @@ const styles = StyleSheet.create({
     fetchingData:{
         color:'black',
         fontWeight:"bold"
-    }
+    },
+    headerContainer:{
+      backgroundColor:"black",
+      height:45,
+      display:"flex",
+      flexDirection:"row",
+      alignItems:"center",
+      paddingHorizontal:20,
+    },
+    text1:{
+      color:Color.brightOrange,
+      flex:0.3
+    },
+    text2:{
+      color:Color.brightOrange,
+      flex:1,
+      marginLeft:10,
+      textAlign:"center"
+    },
+    mainIndividual:{
+      paddingHorizontal:20,
+      borderBottomColor:Color.brightOrange,
+      borderBottomWidth:1
+    },
+    individual:{
+      height:45,
+      display:"flex",
+      flexDirection:'row',
+      alignItems:"center",
+    
+  },
+  dataText:{
+    fontSize:14,
+    color:'black',
+    flex:0.4
+  },
+  dataText2:{
+      fontSize:14,
+      color:'black',
+      flex:0.6,
+      textAlign:"center"
+  },
+  opentext:{
+    color:"#FE98A9",
+    paddingBottom:7,
+    marginTop:-10
+  },
+  allData:{
+    paddingBottom:140
+  }
+
 })
