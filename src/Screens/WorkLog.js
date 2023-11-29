e485d6d58d885e0b7525f1b3adf399862ece8002
abdullahnaveed011 <141331194+abdullahnaveed011@users.noreply.github.com>
2023-11-29 22:33:38 +0500

    changes

--- src/Screens/WorkLog.js
+++ src/Screens/WorkLog.js
@@ -1,9 +1,43 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, FlatList } from 'react-native'
 import { vw,vh } from '../utils/ScreenSize';
+import { MaterialIcons } from '@expo/vector-icons';
 import Color from '../Color';
 const WorkLog = () => {
-  const data = [
+
+  const [selectedTitle, setSelectedTitle] = useState("Un-Categorised");
+  const data=[
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent"
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent"
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent"
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent"
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent..."
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent ...... Trent"
+    },
+    {
+        jobName:"Halo therapy",
+        company: "Sevent Trent"
+    },
+]
+
+  const options = [
     {
       img: require('../../assets/imgs/ic_project_severn_trent.png'),
       title: "Un-Categorised",
@@ -22,7 +56,6 @@ const WorkLog = () => {
     },
   ];
 
-  const [selectedTitle, setSelectedTitle] = useState("");
 
   const renderItem = ({ item, index }) => {
     return (
@@ -40,7 +73,7 @@ const WorkLog = () => {
     <View style={styles.container}>
       {/* Horizontal FlatList */}
       <FlatList
-        data={data}
+        data={options}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
         horizontal
@@ -52,6 +85,32 @@ const WorkLog = () => {
       <View style={styles.selectedCardContainer}>
         <Text style={styles.selectedCardText}>{selectedTitle}</Text>
       </View>
+
+         {/* Header */}
+         <View style={styles.containerHeader}>
+            <Text style={styles.text}>Job Name</Text>
+            <Text style={[styles.text2]}>Company</Text>
+          </View>
+          {/* All Data */}
+          <View>
+            <ScrollView>
+              {
+                data.map((item,index)=>
+                {
+                    return(
+                        <TouchableOpacity onPress={()=>navigation.navigate("jobs-detail",{items: item})} activeOpacity={0.6} key={index}>
+                            <View style={[styles.individual,{backgroundColor:index%2==0 ? '#D2CBBC' : '#F2F1CF'}]}>
+                            <Text style={styles.dataText}>{item.jobName}</Text>
+                            <Text style={styles.dataText2}>{item.company}</Text> 
+                            <MaterialIcons name={'keyboard-arrow-right'} size={28} color="black" />
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })
+              }
+            </ScrollView>
+          </View>
+      
     </View>
   );
 };
@@ -97,6 +156,48 @@ const styles = StyleSheet.create({
     fontSize: 17,
     textAlign:"left"
   },
+  containerHeader:{
+    backgroundColor:"#B1B1B1",
+    height:45,
+    display:"flex",
+    flexDirection:"row",
+    paddingHorizontal:20,
+    alignItems:"center"
+},
+text:{
+    fontSize:16,
+    color:'black',
+    fontWeight:'bold',
+    flex:0.25
+},
+text2:{
+    fontSize:16,
+    color:'black',
+    fontWeight:'bold',
+    flex:0.75,
+    textAlign:"center"
+},
+dataText:{
+    fontSize:14,
+    color:'black',
+    flex:0.4
+},
+dataText2:{
+    fontSize:14,
+    color:'black',
+    flex:0.6,
+    textAlign:"center"
+},
+individual:{
+    height:45,
+    display:"flex",
+    flexDirection:'row',
+    alignItems:"center",
+    paddingHorizontal:20,
+    borderBottomColor:Color.brightOrange,
+    borderBottomWidth:1
+},
+
 });
 
 export default WorkLog;
