36a08cac8e03df085e8006dd4eb84b1d18dd3dc4
abdullahnaveed011 <codewithan@gmail.com>
2025-03-03 17:47:00 +0500

    changes

--- src/Screens/MyNotes.js
+++ src/Screens/MyNotes.js
@@ -2,8 +2,11 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   ActivityIndicator,
+  TouchableOpacity,
+  Alert,
+  ToastAndroid,
+  Platform,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
@@ -11,12 +14,14 @@ import { ScrollView, TextInput } from "react-native-gesture-handler";
 import { URL } from "../utils/Constant";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import Toast from "react-native-toast-message";
+
 const MyNotes = ({ navigation, route }) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { items, Alldata, screenName } = route.params;
-
-  console.log(items, "items...........");
+  const { items } = route.params;
+  const [text, SetText] = useState("");
+  const [updating, setUpdating] = useState(false);
 
   const formatDate = (dateString) => {
     if (!dateString) return "Date not available";
@@ -25,82 +30,149 @@ const MyNotes = ({ navigation, route }) => {
   };
 
   useEffect(() => {
-    (async () => {
+    fetchNotes();
+  }, [items]);
+
+  const fetchNotes = async () => {
+    const authToken = await AsyncStorage.getItem("token");
+    axios
+      .get(URL + `/task/${Number(items.project_id)}/show-mynotes`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          Accept: "application/json",
+        },
+      })
+      .then((res) => {
+        setLoading(false);
+        let notes = res.data.note;
+        setData(notes);
+        if (notes?.note_description) {
+          SetText(notes?.note_description);
+        }
+        console.log(items.project_id, "items.project_id");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const showSimpleAlert = (title, message) => {
+    if (Platform.OS === "android") {
+      ToastAndroid.show(message, ToastAndroid.SHORT);
+    } else {
+      Alert.alert(title, message);
+    }
+  };
+  const handleAdd_UpdatedNotes = async () => {
+    setUpdating(true);
+    console.log("reached");
+    try {
       const authToken = await AsyncStorage.getItem("token");
-      axios
-        .get(URL + `/task/${Number(items.project_id)}/show-mynotes`, {
+      await axios.post(
+        URL + `/task/${Number(items.project_id)}/update-mynotes`,
+        {
+          task_mynotes: text,
+        },
+        {
           headers: {
             Authorization: `Bearer ${authToken}`,
             Accept: "application/json",
           },
-        })
-        .then((res) => {
-          setLoading(false);
-          setData(res.data.note);
-          console.log(items.project_id, "items.project_id");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })();
-  }, [items]);
+        }
+      );
+      await fetchNotes();
+      setUpdating(false);
+      showSimpleAlert(
+        "Success!",
+        text ? "Notes updated successfully." : "Notes added successfully."
+      );
+    } catch (error) {
+      console.log(error);
+      setUpdating(false);
+      showSimpleAlert("Error", "Failed to update notes. Try again?");
+    }
+  };
 
-  return loading ? (
-    <View style={styles.loaderContainer}>
-      <ActivityIndicator color={"black"} size={"large"} />
-      <Text>Fetching Data...</Text>
-    </View>
-  ) : (
-    <ScrollView style={{ flex: 1, backgroundColor: "#EEE5DC" }}>
-      <View style={styles.maiNContainer}>
-        <View style={styles.header}>
-          <Text
-            style={styles.txt}
-          >{`Assigned User: ${items?.assigned[0]?.first_name}`}</Text>
-          <Text style={styles.txt}>{`Stated Data: ${formatDate(
-            items.project_date_start
-          )}`}</Text>
+  return (
+    <>
+      <Toast />
+      {loading ? (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator color={"black"} size={"large"} />
+          <Text>Fetching Data...</Text>
         </View>
-        <View style={styles.container}>
-          <View style={styles.containerTop}>
-            <MaterialCommunityIcons
-              name="calendar-edit"
-              size={29}
-              color="black"
-            />
-            <Text style={styles.txt2}>My Notes</Text>
-          </View>
-          <TextInput
-            placeholder="Write text.."
-            style={styles.input}
-            multiline={true}
-            textAlignVertical="top"
-            selectionColor={"#382504"}
-          />
-          <View style={styles.individualMain}>
-            <View style={styles.individual}>
-              <View style={styles.individualLeft}>
-                <View style={styles.numContainer}>
-                  <Text style={styles.num}>1</Text>
-                </View>
-                <Text style={styles.txt5}>{data?.note_title || "N/A"}</Text>
+      ) : (
+        <ScrollView style={{ flex: 1, backgroundColor: "#EEE5DC" }}>
+          <View style={styles.maiNContainer}>
+            <View style={styles.header}>
+              <Text
+                style={styles.txt}
+              >{`Assigned User: ${items?.assigned[0]?.first_name}`}</Text>
+              <Text style={styles.txt}>{`Stated Data: ${formatDate(
+                items.project_date_start
+              )}`}</Text>
+            </View>
+            <View style={styles.container}>
+              <View style={styles.containerTop}>
+                <MaterialCommunityIcons
+                  name="calendar-edit"
+                  size={29}
+                  color="black"
+                />
+                <Text style={styles.txt2}>My Notes</Text>
               </View>
-              {/* <TouchableOpacity style={styles.icon}>
+              <TextInput
+                placeholder="Write text.."
+                style={styles.input}
+                multiline={true}
+                textAlignVertical="top"
+                selectionColor={"#382504"}
+                value={text}
+                onChangeText={(txt) => SetText(txt)}
+              />
+              <TouchableOpacity
+                onPress={() => handleAdd_UpdatedNotes()}
+                style={styles.btn}
+                activeOpacity={0.6}
+              >
+                <Text style={styles.btnTxt}>
+                  {text
+                    ? updating
+                      ? "Updating Notes..."
+                      : "Update Notes"
+                    : updating
+                    ? "Adding Notes..."
+                    : "Add Notes"}
+                </Text>
+              </TouchableOpacity>
+              <View style={styles.individualMain}>
+                <View style={styles.individual}>
+                  <View style={styles.individualLeft}>
+                    <View style={styles.numContainer}>
+                      <Text style={styles.num}>1</Text>
+                    </View>
+                    {data?.note_title && (
+                      <Text style={styles.txt5}>
+                        {data?.note_title || "N/A"}
+                      </Text>
+                    )}
+                  </View>
+                  {/* <TouchableOpacity style={styles.icon}>
                 <MaterialCommunityIcons
                   name="delete-outline"
                   size={20}
                   color="black"
                 />
               </TouchableOpacity> */}
-            </View>
-            <Text style={styles.smTxt}>{data?.note_description}</Text>
-            <View style={styles.dateWrapper}>
-              <Text style={styles.date}>
-                {data?.note_created?.slice(0, 10) || "N/A"}
-              </Text>
-            </View>
-          </View>
-          {/* <View style={styles.fileContainer}>
+                </View>
+                <Text style={styles.smTxt}>{data?.note_description}</Text>
+                <View style={styles.dateWrapper}>
+                  <Text style={styles.date}>
+                    {formatDate(data?.note_updated?.slice(0, 10)) || "N/A"}
+                  </Text>
+                </View>
+              </View>
+              {/* <View style={styles.fileContainer}>
             {Array(5)
               .fill(0)
               .map((item, index) => {
@@ -135,9 +207,11 @@ const MyNotes = ({ navigation, route }) => {
                 );
               })}
           </View> */}
-        </View>
-      </View>
-    </ScrollView>
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </>
   );
 };
 
@@ -224,7 +298,7 @@ const styles = StyleSheet.create({
     fontSize: 13,
     borderWidth: 1,
     borderColor: "lightgray",
-    height: 120,
+    minHeight: 120,
   },
   smTxt: {
     fontSize: 11,
@@ -249,5 +323,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 5,
     marginTop: -100,
+  },
+  btn: {
+    backgroundColor: "black",
+    marginLeft: "auto",
+    paddingVertical: 9,
+    paddingHorizontal: 20,
+    borderRadius: 1000,
+    marginTop: -5,
+  },
+  btnTxt: {
+    color: "white",
   },
 });
