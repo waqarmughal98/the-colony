a35d2196163a681f30e4db405aa801e9062c4030
Mohad Naeem <waqar.ali@napollo.net>
2025-03-24 16:28:12 +0500

    Sort

--- src/Screens/Tasks.js
+++ src/Screens/Tasks.js
@@ -1,64 +1,64 @@
-import { StyleSheet, Text, View ,Alert,ActivityIndicator} from 'react-native'
-import React,{useState, useEffect} from 'react'
+import { StyleSheet, Text, View, Alert, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import TaskContainer from '../Components/Tasks/TaskContainer'
 import { URL } from '../utils/Constant';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const Tasks = ({navigation,route}) => {
-    const [loading, setLoading]=useState(true)
-    const {Task}=route.params;
+const Tasks = ({ navigation, route }) => {
+    const [loading, setLoading] = useState(true)
+    const { Task } = route.params;
     const [data, setData] = useState([]);
     React.useLayoutEffect(() => {
-       navigation.setOptions({
-          headerTintColor: 'white',
-          headerTitleAlign: 'center',
-          headerTitle: "All Tasks",
+        navigation.setOptions({
+            headerTintColor: 'white',
+            headerTitleAlign: 'center',
+            headerTitle: "All Tasks",
         });
-      }, [navigation]);
+    }, [navigation]);
 
-    useEffect(()=>{
-        (async ()=>{
+    useEffect(() => {
+        (async () => {
             const authToken = await AsyncStorage.getItem("token");
             axios.get(URL + '/task/all', {
                 headers: {
                     Authorization: `Bearer ${authToken}`
                 }
-            }).then((res)=>{
-                // console.log(res.data.tasks.data, 'All Task');
+            }).then((res) => {
+                console.log(res.data.tasks.data, 'All Task');
                 setData(res.data.tasks.data);
                 setLoading(false)
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err);
             })
         })()
-    },[Task])
-  return (
-    <View>
-        {
-        !loading ? 
-            <View>
-                <TaskContainer screenName={"tasks"} navigation={navigation} data={data}/>
-            </View>
-            :
-            <View style={styles.Indicator}>
-                <ActivityIndicator size="large" color={"black"} />
-                <Text style={styles.fetchingData}>Fetching Data</Text>
-            </View>
-        }
-     </View>
-  )
+    }, [Task])
+    return (
+        <View>
+            {
+                !loading ?
+                    <View>
+                        <TaskContainer screenName={"tasks"} navigation={navigation} data={data} />
+                    </View>
+                    :
+                    <View style={styles.Indicator}>
+                        <ActivityIndicator size="large" color={"black"} />
+                        <Text style={styles.fetchingData}>Fetching Data</Text>
+                    </View>
+            }
+        </View>
+    )
 }
 
 export default Tasks
 
 const styles = StyleSheet.create({
-    Indicator:{
+    Indicator: {
         height: '90%',
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        alignContent:"center"
-      },
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center"
+    },
 })
